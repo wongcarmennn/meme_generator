35bbf2ce7101dbbd666a6d5779ca629f9874fe5b
Carmen Wong <carmenwong@192.168.1.100>
2025-07-03 11:35:22 +0100

    Project 1: Meme generator

---
+++ components/Practice.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Practice() {
+    return (
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Practice Commit Squashing</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+            <h5 className="header--project">Changes made 111</h5>
+            <h6 className="header--project">Changes made 222</h6>
+            <h7 className="header--project">Changes made 333</h7>
+            <h8 className="header--project">TO KEEP THIS FINAL CHANGE</h8>
+        </header>
+    )
+}
